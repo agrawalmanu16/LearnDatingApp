b577156842742b7a4251c460ef39f1fa7c00a50d
Manu Agarwal <magarwal7@asiapac.globalcsc.net>
2019-02-25 16:09:53 +0530

    End of section 8

--- DatingApp.API/Helpers/Extensions.cs
+++ DatingApp.API/Helpers/Extensions.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.AspNetCore.Http;
 
 namespace DatingApp.API.Helpers
@@ -9,5 +10,17 @@ namespace DatingApp.API.Helpers
             response.Headers.Add("Access-Control-Expose-Headers", "Application-Error");
             response.Headers.Add("Access-Control-Allow-Origin", "*");
         }
+
+        public static int CalculateAge(this DateTime thisDateTime)
+        {
+            int age = DateTime.Today.Year - thisDateTime.Year;
+            if(thisDateTime.AddYears(age) > DateTime.Today)
+              age--;
+            return age;
+            
+        }
+
     }
+
+   
 }
